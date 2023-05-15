80f0df27701a8b5814ffd3cc77c5743fc35f82cd
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2023-05-15 17:02:33 +0200

    Fix Formatting

--- trader/src/app.rs
+++ trader/src/app.rs
@@ -13,23 +13,22 @@ use crate::{
     },
 };
 
-
 fn load_icon() -> eframe::IconData {
-	let (icon_rgba, icon_width, icon_height) = {
-		let icon = include_bytes!("../res/icon.png");
-		let image = image::load_from_memory(icon)
-			.expect("Failed to open icon path")
-			.into_rgba8();
-		let (width, height) = image.dimensions();
-		let rgba = image.into_raw();
-		(rgba, width, height)
-	};
-	
-	eframe::IconData {
-		rgba: icon_rgba,
-		width: icon_width,
-		height: icon_height,
-	}
+    let (icon_rgba, icon_width, icon_height) = {
+        let icon = include_bytes!("../res/icon.png");
+        let image = image::load_from_memory(icon)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    eframe::IconData {
+        rgba: icon_rgba,
+        width: icon_width,
+        height: icon_height,
+    }
 }
 
 pub fn gui_main(
