6edf8d2767bebca7610f6f9d08304050d89a9c90
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2023-05-17 14:11:36 +0200

    Simple LOD System

--- trader/src/windows/world_explorer.rs
+++ trader/src/windows/world_explorer.rs
@@ -4,6 +4,7 @@ use crate::backend::Command;
 use crate::backend::CommandRequest;
 use crate::utils::ExpectLock;
 use egui::*;
+use egui::plot::{MarkerShape, Points};
 use plot::{Corner, Legend, Plot, PlotPoint, Text};
 #[derive(Debug, Default)]
 pub struct WorldExplorerData {
@@ -33,29 +34,52 @@ impl ControlWindow for WorldExplorerData {
             }
             if let Some(universe_list) = &trading_gui.game_data.universe_data {
                 plot.show(ui, |plot_ui| {
+                    // Here is some code for debugging the plot bounds:
+                    // println!("{:?}", plot_ui.plot_bounds().width());
+                    //
+                    // After zooming in to about a scale of less that 1500
+                    // start rendering text
+                    const TEXT_RENDER_LEVEL: f64 = 500.0; 
+                    let render_text = plot_ui.plot_bounds().width() < TEXT_RENDER_LEVEL;
+
                     for system in universe_list {
                         if systems_to_render.contains(&&system.symbol)
                             || !self.only_show_systems_with_ships
                         {
-                            plot_ui.text(
-                                Text::new(PlotPoint::new(system.y, system.x), &system.symbol)
-                                    .name("System")
-                                    .color(Color32::RED),
-                            );
-                            for waypoint in &system.waypoints {
+                            if render_text {
                                 plot_ui.text(
-                                    Text::new(
-                                        PlotPoint::new(
-                                            // Waypoints usualy spread around 200 units around their base system
-                                            // and systems are usually 2 units apart at the core of the galaxy
-                                            system.y as f64 + (waypoint.y as f64 / 200.0) * 2.0,
-                                            system.x as f64 + (waypoint.x as f64 / 200.0) * 2.0,
-                                        ),
-                                        &waypoint.symbol,
-                                    )
-                                    .name("Waypoint")
-                                    .color(Color32::BLUE),
+                                    Text::new(PlotPoint::new(system.y, system.x), &system.symbol)
+                                        .name("System")
+                                        .color(Color32::RED),
                                 );
+                            }else {
+                                let points = Points::new(vec![[system.y as f64, system.x as f64]])
+                                    .radius(
+                                        (map_range(
+                                            (TEXT_RENDER_LEVEL, 20000.0),
+                                            (6.0, 2.0),
+                                            plot_ui.plot_bounds().width()
+                                        ) as f32).max(2.0))
+                                    .shape(MarkerShape::Asterisk);
+                                plot_ui.points(points);
+                            }
+
+                            if render_text {
+                                for waypoint in &system.waypoints {
+                                    plot_ui.text(
+                                        Text::new(
+                                            PlotPoint::new(
+                                                // Waypoints usualy spread around 200 units around their base system
+                                                // and systems are usually 2 units apart at the core of the galaxy
+                                                system.y as f64 + (waypoint.y as f64 / 200.0) * 2.0,
+                                                system.x as f64 + (waypoint.x as f64 / 200.0) * 2.0,
+                                            ),
+                                            &waypoint.symbol,
+                                        )
+                                        .name("Waypoint")
+                                        .color(Color32::BLUE),
+                                    );
+                                }
                             }
                         }
                     }
@@ -103,4 +127,8 @@ impl ControlWindow for WorldExplorerData {
     fn visibility(&mut self) -> &mut bool {
         &mut self.visible
     }
+}
+
+fn map_range(from_range: (f64, f64), to_range: (f64, f64), s: f64) -> f64 {
+    to_range.0 + (s - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
 }
